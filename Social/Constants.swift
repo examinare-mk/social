01e1f25e9cc362b78b0e51f34798130e70f883ca
Mike Verpuchinskiy <mike@MikeBook.local>
2016-11-04 11:50:28 +0200

    Sign in ui custom classes complete

---
+++ Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social
+//
+//  Created by Mike Verpuchinskiy on 11/4/16.
+//  Copyright © 2016 MIKE VERPUCHINSKIY. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0/255
