ae4a56e0ac1932f76c91fbff59a1737287b621c4
Mike Verpuchinskiy <mike@MikeBook.local>
2016-11-04 22:15:05 +0200

    Added UI for news feed

--- Social/CircleView.swift
+++ Social/CircleView.swift
@@ -1,5 +1,5 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  Social
 //
 //  Created by Mike Verpuchinskiy on 11/4/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,7 +17,12 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
