59e294d03f50b5cbc536bd72597bca4273d6ebd0
Mike Verpuchinskiy <mike@MikeBook.local>
2016-11-06 22:41:16 +0200

    Complete Firebase storage download

--- Social/PostCell.swift
+++ Social/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -23,11 +24,31 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLabel.text = "\(post.likes)"
+        
+        if img != nil {
+            self.postImg.image = img
+        } else {
+                let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+                ref.data(withMaxSize: 2 * 1024 * 1024, completion: {(data, error) in
+                    if error != nil {
+                        print("Unable to download image from Firebase storage")
+                    } else {
+                        print("Image downloaded from Firebase storage")
+                        if let imageData = data {
+                            if let image = UIImage(data: imageData) {
+                                self.postImg.image = image
+                                FeedVC.imageCache.setObject(image, forKey: post.imageUrl as NSString)
+                            }
+                        }
+                    }
+                })
+            }
+        }
     }
 
 
-}
+
